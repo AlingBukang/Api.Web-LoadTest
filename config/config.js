ec5339933070d5535e6846fb99fa5207c2584c2a
AlingBukang <46798841+AlingBukang@users.noreply.github.com>
2022-07-20 11:49:21 +1200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     // "environment": "eco1",
-    "userName": "api_automation@test.xero.com",
-    "password": "p4ssw0rd",
+    "userName": "",
+    "password": "",
     // "URL": "https://api.eco1.xero-test.com",
-    //  "CONSUMER_KEY": "NSWCHBR1Z11SJHEPGXTMKDCN5B9KOL",
-    // "URL": "https://api.livestage6.test.xero.com",
-    "CONSUMER_KEY": "K2MNS3SYOMSH6M1FXNLVX4IAJNEVTE",
-    "privateKeyPath": "certs/privatekey.pem"
+    //  "CONSUMER_KEY": "L",
+    // "URL": "",
+    "CONSUMER_KEY": "",
+    "privateKeyPath": "certs/"
 }
